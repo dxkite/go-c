85d04cc052c3e566daf6f8618eaddfafeeb9f9e9
dxkite <dxkite@163.com>
2021-02-08 22:09:00 +0800

    添加：CONCAT

--- preprocess/preprocess.go
+++ preprocess/preprocess.go
@@ -4,6 +4,7 @@ import (
 	"dxkite.cn/go-c11"
 	"dxkite.cn/go-c11/scanner"
 	"dxkite.cn/go-c11/token"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -110,7 +111,6 @@ func (c *Context) Scan() (t token.Token) {
 		}
 		// 宏展开
 		if c.doExpand(c.cur) {
-			c.next()
 			continue
 		}
 		// 普通token
@@ -142,6 +142,17 @@ func (c *Context) next() {
 		c.tok = token.EOF
 		c.lit = ""
 	}
+}
+
+func (c *Context) record() {
+	c.rcd = true
+	c.tks = c.tks[0:0]
+}
+
+func (c *Context) arr() []token.Token {
+	pp := []token.Token{}
+	pp = append(pp, c.tks...)
+	return pp
 }
 
 // 获取下一个非空token
@@ -305,8 +316,7 @@ func (c *Context) doExpand(tok token.Token) bool {
 	if tok.Type() != token.IDENT {
 		return false
 	}
-	if tks, ok := c.Expand(tok); ok {
-		c.push(tks)
+	if _, ok := c.Expand(tok); ok {
 		return true
 	}
 	return false
@@ -324,49 +334,142 @@ func (c *Context) Expand(tok token.Token) ([]token.Token, bool) {
 	}
 	name := tok.Literal()
 	if v, ok := c.Val[name]; ok {
-		// 宏未定义时不展开
-		if t, ok := tok.(*Token); ok && v.Index > t.Index {
-			return nil, false
-		}
 		switch val := v.Decl.(type) {
 		case *MacroVal:
-			return c.ExpandVal(tok, v.Index, val.Body), true
+			// 宏未定义时不展开
+			if t, ok := tok.(*Token); ok && v.Index > t.Index {
+				return nil, false
+			}
+			tks := c.ExpandVal(tok, v.Index, val.Body, nil)
+			c.push(tks)
+			c.next()
+			return tks, true
 		case *MacroHandler:
-			return c.ExpandVal(tok, v.Index, val.Handler(tok)), true
+			// 宏未定义时不展开
+			if t, ok := tok.(*Token); ok && v.Index > t.Index {
+				return nil, false
+			}
+			tks := c.ExpandVal(tok, v.Index, val.Handler(tok), nil)
+			c.push(tks)
+			c.next()
+			return tks, true
 		case *MacroFunc:
 			// 忽略非函数式宏
 			if n := c.peekNext(); n.Literal() != "(" {
 				return nil, false
 			}
+			// 宏未定义时不展开
+			if !c.canExpandMacroFunc(tok) {
+				return nil, false
+			}
 			// 处理
+			if tks, ok := c.ExpandFunc(tok, v.Index, val); ok {
+				tks = append(tks, c.cur)
+				c.push(tks)
+				c.next()
+				return tks, true
+			} else {
+				return nil, false
+			}
 		}
 	}
 	return nil, false
 }
 
+func (c *Context) canExpandMacroFunc(tok token.Token) bool {
+	var exp token.Token
+	// 获取展开历史
+	if t, ok := tok.(*Token); ok && t.Expand != nil {
+		exp = t.Expand
+	} else {
+		return true
+	}
+	// 不递归展开
+	for {
+		if exp.Literal() == tok.Literal() {
+			return false
+		}
+		if t, ok := exp.(*Token); ok && t.Expand != nil {
+			exp = t.Expand
+		} else {
+			return true
+		}
+	}
+}
+
 // 展开宏
-func (c *Context) ExpandVal(v token.Token, idx int, tks []token.Token) []token.Token {
-	ex := make([]token.Token, len(tks))
+func (c *Context) ExpandVal(v token.Token, idx int, tks []token.Token, params map[string][]token.Token) []token.Token {
+	ex := make([]token.Token, 0)
 	col := 0
 	pos := v.Position()
-	for i, tok := range tks {
-		if i == 0 {
+	ps := scanner.NewPeekScan(scanner.NewTokenScan(scanner.NewArrayScan(tks)))
+	f := true
+
+	for tok := ps.Scan(); tok.Type() != token.EOF; tok = ps.Scan() {
+		if f {
 			col = tok.Position().Column
+			f = false
 		}
+
+		typ := tok.Type()
+		lit := tok.Literal()
+
+		// ## 操作
+		if ps.PeekOne().Literal() == "##" {
+			ps.Scan() //##
+			nxt := ps.Scan()
+			lit = tok.Literal() + nxt.Literal()
+			if !isValidToken(lit) {
+				typ = token.ILLEGAL
+				c.err.Add(c.pos, "invalid ## operator between %s and %s", lit, nxt.Literal())
+			}
+		}
+
+		if tok.Literal() == "#" &&
+			params != nil && ps.PeekOne().Type() == token.IDENT {
+			name := ps.PeekOne().Literal()
+			typ = token.STRING
+			if v, ok := params[name]; ok {
+				ps.Scan()
+				lit = strconv.QuoteToGraphic(token.RelativeString(v))
+			}
+		}
+
+		if tok.Type() == token.IDENT && params != nil {
+			if v, ok := params[tok.Literal()]; ok {
+				ms := scanner.NewMultiScan(ps)
+				ms.Push(scanner.NewArrayScan(v))
+				ps = scanner.NewPeekScan(ms)
+				continue
+			}
+		}
+
 		t := &Token{
 			Pos: token.Position{
 				Filename: pos.Filename,
 				Line:     pos.Line,
 				Column:   pos.Column + tok.Position().Column - col,
 			},
-			Typ:    tok.Type(),
-			Lit:    tok.Literal(),
+			Typ:    typ,
+			Lit:    lit,
 			Index:  idx,
 			Expand: v,
 		}
-		ex[i] = t
+		ex = append(ex, t)
 	}
 	return ex
+}
+
+func isValidToken(lit string) bool {
+	s := scanner.NewStringScan("<runtime>", lit)
+	tok := scanner.ScanToken(s)
+	if len(tok) > 1 {
+		return false
+	}
+	if s.Error().Len() > 0 {
+		return false
+	}
+	return true
 }
 
 // peek 下一个非空 token
@@ -380,23 +483,13 @@ func (c *Context) peekNext() token.Token {
 		if v[n-1].Type() != token.WHITESPACE {
 			return v[n-1]
 		}
+		n++
 	}
 	return &Token{
 		Pos: token.Position{},
 		Typ: token.EOF,
 		Lit: "",
 	}
-}
-
-// peek 下一个 token
-func (c *Context) peekOne() token.Token {
-	if ps, ok := c.in.(scanner.PeekScanner); ok {
-		return ps.PeekOne()
-	}
-	p := scanner.NewPeekScan(c.in)
-	tok := p.PeekOne()
-	c.in = scanner.NewMultiScan(p)
-	return tok
 }
 
 func (c *Context) peek(offset int) []token.Token {
@@ -417,6 +510,19 @@ func (c *Context) expectIdent() string {
 	}
 	c.err.Add(c.pos, fmt.Sprintf("expect token %s got %s", token.IDENT, c.tok))
 	return ""
+}
+
+func (c *Context) expectPunctuator(lit string) {
+	c.punctuator(lit, true)
+}
+
+func (c *Context) punctuator(lit string, require bool) {
+	if c.tok == token.PUNCTUATOR && lit == c.lit {
+		c.nextToken()
+	}
+	if require {
+		c.err.Add(c.pos, fmt.Sprintf("expect punctuator %s got %s", lit, c.lit))
+	}
 }
 
 func (c *Context) expectEndMacro() {
@@ -509,7 +615,7 @@ func (c *Context) evalConstExpr() bool {
 func (c *Context) doDefine() {
 	c.nextToken()
 	ident := c.expectIdent()
-	if n := c.peekOne(); n.Literal() == "(" {
+	if c.lit == "(" {
 		c.doDefineFunc(ident)
 	} else {
 		c.doDefineVal(ident)
@@ -538,12 +644,146 @@ func (c *Context) doDefineVal(ident string) {
 		tks = append(tks, c.cur)
 		c.nextToken()
 	}
+
+	if pos, err := checkValidMacroExpr(tks); err != nil {
+		c.err.Add(pos, err.Error())
+		return
+	}
+
 	c.Define(ident, &MacroVal{
 		Name: ident,
 		Body: tks,
 	})
 }
 
-func (c *Context) doDefineFunc(ident string) {
+// 检查是否可用
+func checkValidMacroExpr(tks []token.Token) (token.Position, error) {
+	if len(tks) > 0 {
+		beg := 0
+		end := len(tks) - 1
+		err := errors.New("'##' cannot appear at either end of a macro expansion")
+		if tks[beg].Literal() == "##" {
+			return tks[beg].Position(), err
+		}
+		if tks[end].Literal() == "##" {
+			return tks[end].Position(), err
+		}
+	}
+	return token.Position{}, nil
+}
 
+func (c *Context) doDefineFunc(ident string) {
+	tks := make([]token.Token, 0)
+	params := make([]string, 0)
+	c.expectPunctuator("(")
+
+	elp := false
+	for !c.isMacroEnd() && c.lit != ")" {
+		if c.lit == "..." {
+			elp = true
+			c.nextToken()
+			break
+		} else if c.tok == token.IDENT {
+			params = append(params, c.lit)
+			c.nextToken()
+			c.punctuator(",", false)
+		} else {
+			c.err.Add(c.pos, fmt.Sprintf("expect ident, got %s <%s>", c.tok, c.lit))
+			break
+		}
+	}
+
+	c.expectPunctuator(")")
+
+	for !c.isMacroEnd() {
+		tks = append(tks, c.cur)
+		c.nextToken()
+	}
+
+	if pos, err := checkValidMacroExpr(tks); err != nil {
+		c.err.Add(pos, err.Error())
+		return
+	}
+
+	c.Define(ident, &MacroFunc{
+		Name:     ident,
+		Params:   params,
+		Ellipsis: elp,
+		Body:     tks,
+	})
+}
+
+// 展开函数
+func (c *Context) ExpandFunc(tok token.Token, index int, val *MacroFunc) ([]token.Token, bool) {
+	c.record()
+	c.nextToken()
+	params, ok := c.readParameters(val)
+	scan := c.arr()
+	// 参数错误不解析
+	if !ok {
+		scan = append(scan, c.cur)
+		c.push(scan)
+		c.next()
+		return nil, false
+	}
+	return c.ExpandVal(tok, index, val.Body, params), true
+}
+
+func (c *Context) readParameters(val *MacroFunc) (map[string][]token.Token, bool) {
+	c.expectPunctuator("(")
+	params := map[string][]token.Token{}
+	i := 0
+	lp := len(val.Params)
+	for !c.isMacroEnd() && c.lit != ")" {
+		if len(params) < lp {
+			p := c.readParameter()
+			params[val.Params[i]] = p
+			c.punctuator(",", i+1 < lp)
+		} else if val.Ellipsis {
+			params["__VA_ARGS__"] = c.readEllipsisParameter()
+		} else {
+			c.err.Add(c.pos, "expect params %d got %d", lp, i)
+		}
+		i++
+	}
+	c.expectPunctuator(")")
+	if len(params) < lp {
+		c.err.Add(c.pos, "requires %d arguments, but only %d given", lp, len(params))
+		return nil, false
+	}
+	return params, true
+}
+
+// 读取参数
+func (c *Context) readParameter() []token.Token {
+	c.record()
+	paren := 0
+	for !c.isMacroEnd() && c.lit != "," && c.lit != ")" {
+		if c.lit == "(" {
+			paren++
+		}
+		if c.lit == ")" && paren != 0 {
+			paren--
+			c.nextToken()
+		}
+		c.nextToken()
+	}
+	return c.arr()
+}
+
+// 读取参数
+func (c *Context) readEllipsisParameter() []token.Token {
+	c.record()
+	paren := 0
+	for !c.isMacroEnd() && c.lit != ")" {
+		if c.lit == "(" {
+			paren++
+		}
+		if c.peekNext().Literal() == ")" && paren > 0 {
+			paren--
+			c.nextToken()
+		}
+		c.nextToken()
+	}
+	return c.arr()
 }
