bd3f08998b2839c9e9e408fac448e7d7423dcf21
dxkite <dxkite@163.com>
2021-02-08 21:08:30 +0800

    添加：相对token转字符串

--- token/token_test.go
+++ token/token_test.go
@@ -43,3 +43,26 @@ func TestString(t *testing.T) {
 		t.Errorf("want %s got %s\n", strconv.QuoteToGraphic(want), strconv.QuoteToGraphic(got))
 	}
 }
+
+func TestRelativeString(t *testing.T) {
+	startLine := 10
+
+	tks := []Token{
+		&token{Lit: "a", Pos: Position{Filename: "", Line: 1 + startLine, Column: 1}},
+		&token{Lit: "中", Pos: Position{Filename: "", Line: 1 + startLine, Column: 2}},
+		&token{Lit: "♥", Pos: Position{Filename: "", Line: 1 + startLine, Column: 3}},
+		&token{Lit: "\n", Typ: NEWLINE, Pos: Position{Filename: "", Line: 1 + startLine, Column: 4}},
+		&token{Lit: "\n", Typ: NEWLINE, Pos: Position{Filename: "", Line: 2 + startLine, Column: 1}},
+		&token{Lit: "a", Pos: Position{Filename: "", Line: 3 + startLine, Column: 1}},
+		&token{Lit: "b", Pos: Position{Filename: "", Line: 4 + startLine, Column: 1}},
+		&token{Lit: "\n", Typ: NEWLINE, Pos: Position{Filename: "", Line: 4 + startLine, Column: 2}},
+		&token{Lit: "a", Pos: Position{Filename: "", Line: 5 + startLine, Column: 1}},
+		&token{Lit: "b", Pos: Position{Filename: "", Line: 6 + startLine, Column: 1}},
+	}
+
+	got := RelativeString(tks)
+	want := "a中♥\n\na\nb\na\nb"
+	if want != got {
+		t.Errorf("want %s got %s\n", strconv.QuoteToGraphic(want), strconv.QuoteToGraphic(got))
+	}
+}
