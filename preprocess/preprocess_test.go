da19064f172280a474f1366388d0ecc34c05d4e2
dxkite <dxkite@163.com>
2021-02-12 00:21:22 +0800

    添加：测试用例错误记录

--- preprocess/preprocess_test.go
+++ preprocess/preprocess_test.go
@@ -2,11 +2,13 @@ package preprocess
 
 import (
 	"bytes"
+	go_c11 "dxkite.cn/go-c11"
 	"dxkite.cn/go-c11/scanner"
 	"dxkite.cn/go-c11/token"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -40,25 +42,40 @@ func TestScanFile(t *testing.T) {
 
 				tks := scanner.ScanToken(exp)
 
-				if !exists(p+".json") || !exists(p+"c") {
+				expectToken := p + ".json"
+				expectCode := p + "c"
+				expectErr := p + ".err.json"
 
-					if err := scanner.SaveJson(p+".json", tks); err != nil {
+				if !exists(expectToken) || !exists(expectCode) || !exists(expectErr) {
+
+					if err := scanner.SaveJson(expectToken, tks); err != nil {
 						t.Errorf("SaveJson error = %v", err)
 					}
 
-					if err := ioutil.WriteFile(p+"c", []byte(token.String(tks)), os.ModePerm); err != nil {
+					if err := ioutil.WriteFile(expectCode, []byte(token.String(tks)), os.ModePerm); err != nil {
 						t.Errorf("SaveResult error = %v", err)
+					}
+
+					if err := exp.Error().SaveFile(expectErr); err != nil {
+						t.Errorf("SaveError error = %v", err)
 					}
 					return
 				}
 
-				if data, err := ioutil.ReadFile(p + "c"); err != nil {
+				if data, err := ioutil.ReadFile(expectCode); err != nil {
 					t.Errorf("LoadResult error = %v", err)
 				} else {
 					got := token.String(tks)
 					if !bytes.Equal(data, []byte(got)) {
 						t.Errorf("result error:want:\t%s\ngot:\t%s\n", string(data), got)
 					}
+				}
+
+				var loadError go_c11.ErrorList
+				if err := loadError.LoadFromFile(expectErr); err != nil {
+					t.Errorf("LoadFromFile error = %v", err)
+				} else if !reflect.DeepEqual(loadError, *exp.Error()) {
+					t.Errorf("ScanFile() got = %v, want %v", loadError, exp.Error())
 				}
 			})
 		}
